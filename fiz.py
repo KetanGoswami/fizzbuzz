7efa3efa78ea5ba4123fbee0ff05daa883a21b80
parthpatel041 <Parthkumar.Patel@dal.ca>
2019-05-23 05:41:41 -0700

    imported 1 to 100 and printing fizzBuzz

--- fiz.py
+++ fiz.py
@@ -1,1 +1,13 @@
-print (" new repo")
+#print (" new repo")
+
+for n in range(100):
+    if n % 15 == 0:       #number divisible by 15 (divisible by 3 & 5)
+        print("FizzBuzz")
+        continue
+    elif n % 3 == 0:      #number divisible by 3 print fizz
+        print("Fizz")
+        continue
+    elif n % 5 == 0:      #number divisible by 5 print buzz
+        print("Buzz")
+        continue
+    print(n)
